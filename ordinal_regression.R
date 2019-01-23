96ddbb40cac28b582889a5490f5d1389f24e4f84
WIZ <wilsonzhou92@gmail.com>
2019-01-23 15:26:04 +0100

    found some more invalid char

--- ordinal_regression.R
+++ ordinal_regression.R
@@ -8,8 +8,8 @@ inverse_logit <- function(value){
 
 #   -----------------------------------------------------------------------
 # Probability to be in category j given input variables/ covariates for person i, xi
-# P(Yi=j|xi)=P(Yi???j|xi)???P(Yi???j???1|xi)P(Yi=j|xi)=P(Yi???j|xi)???P(Yi???j???1|xi)
-# where P(Yi???j|xi)=exp(a_j+b^Txi)(1+exp(a_j+b^Txi))P(Yi???j|xi)=exp(a_j+b^Txi)(1+exp(a_j+b^Txi))
+# P(Yi=j|xi)=P(Yi<=j|xi)???P(Yi<=j???1|xi)P(Yi=j|xi)=P(Yi<=j|xi)???P(Yi<=j???1|xi)
+# where P(Yi<=j|xi)=exp(a_j+b^Txi)(1+exp(a_j+b^Txi))P(Yi<=j|xi)=exp(a_j+b^Txi)(1+exp(a_j+b^Txi))
 #   -----------------------------------------------------------------------
 prob_j         <- function(a, j, b, x_i, min_obs_resp, max_obs_resp){
   
